911990b68906f45754d3b5052f156c35615d10d8
Brandon Yi <brandon.yi@virtium.com>
2020-10-19 17:44:14 -0700

    Can download firmware

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char** argv) {
     // takes in a path to a Hyperstone firmware archive and the path to the device to upgrade
     // TODO let user specific device path in the command line args
     const char* devPath = "/dev/sdb";
-    std::string ddPath = "/home/bypie5/Documents/Virtium/hsfmt/bin/U9/dd.txt";
+    std::string ddPath = "/home/bypie5/Downloads/U9/dd.txt";
 
     updater::FirmwareUpdater* updater = new updater::FirmwareUpdater(devPath, ddPath);
     
